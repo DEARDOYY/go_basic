27deb2339ddda081f5ef172a09fb412160b0ccec
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2024-08-17 18:30:56 +0700

    update func write to file

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance) 
+	os.WriteFile("balance.txt", []byte(balanceText), 0644)
+}
 
 func main() {
 	var accountBalance float64 = 1000.0
@@ -16,17 +24,26 @@ func main() {
 		var choice int
 		fmt.Print("Your choice: ")
 		fmt.Scan(&choice)
-	
-		if choice == 1 {
+
+		switch choice {
+		case 1:
 			fmt.Println("Your balance is", accountBalance)
-		} else if choice == 2 {
-			fmt.Print("Your deposit: ")
+		case 2:
+				fmt.Print("Your deposit: ")
 			var depositAmount float64
 			fmt.Scan(&depositAmount)
+
+			if(depositAmount <= 0) {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				continue
+			}
+
 			accountBalance = accountBalance + depositAmount
 			fmt.Println("Balance update! New amount: ", accountBalance)
-		} else if choice == 3 {
-			fmt.Print("Withdraw amount: ")
+
+			writeBalanceToFile(accountBalance)
+		case 3:
+				fmt.Print("Withdraw amount: ")
 			var withdrawAmount float64
 			fmt.Scan(&withdrawAmount)
 	
@@ -42,10 +59,45 @@ func main() {
 	
 			accountBalance = accountBalance - withdrawAmount
 			fmt.Println("Balance update! New amount: ", accountBalance)
-		} else {
+
+			writeBalanceToFile(accountBalance)
+		default:
 			fmt.Println("Goodbye!")
-			break
+			fmt.Println("Thanks for choosing our bank <3")
+
+			return
+			// break
 		}
+	
+		// if choice == 1 {
+		// 	fmt.Println("Your balance is", accountBalance)
+		// } else if choice == 2 {
+		// 	fmt.Print("Your deposit: ")
+		// 	var depositAmount float64
+		// 	fmt.Scan(&depositAmount)
+		// 	accountBalance = accountBalance + depositAmount
+		// 	fmt.Println("Balance update! New amount: ", accountBalance)
+		// } else if choice == 3 {
+		// 	fmt.Print("Withdraw amount: ")
+		// 	var withdrawAmount float64
+		// 	fmt.Scan(&withdrawAmount)
+	
+		// 	if withdrawAmount <= 0 {
+		// 		fmt.Println("Invalid amount. Must be greater than 0.")
+		// 		continue
+		// 	}
+	
+		// 	if withdrawAmount > accountBalance {
+		// 		fmt.Println("Invalid amount. You can't withdraw more than you have.")
+		// 		continue
+		// 	}
+	
+		// 	accountBalance = accountBalance - withdrawAmount
+		// 	fmt.Println("Balance update! New amount: ", accountBalance)
+		// } else {
+		// 	fmt.Println("Goodbye!")
+		// 	break
+		// }
 	}
-	fmt.Println("Thanks for choosing our bank <3")
+	// fmt.Println("Thanks for choosing our bank <3")
 }
